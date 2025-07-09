910f1b7ce3ca14b31fb3450746258af85870bc78
sandesh4747 <sangitthapa328@gmail.com>
2025-07-09 14:00:35 +0545

    changes

--- backend/src/lib/generateTokenAndSetCookie.js
+++ backend/src/lib/generateTokenAndSetCookie.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
