e6ed78932cb7430e34dc5292366776b890a9d290
sandesh4747 <sangitthapa328@gmail.com>
2025-07-09 14:03:19 +0545

    changes

--- frontend/src/app/mainApi.js
+++ frontend/src/app/mainApi.js
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // export const baseUrl = "http://localhost:5002/api";
 
 export const baseUrl =
-  import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5002/api"
+    : import.meta.env.VITE_API_URL; // or REACT_APP_API_URL for CRA
 
 export const mainApi = createApi({
   reducerPath: "mainApi",
